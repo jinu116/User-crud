78d10e25e60c54ecca763a93339a5f10f084c902
jinu116 <45252163+jinu116@users.noreply.github.com>
2024-10-23 22:44:29 -0700

    Create UserEntity.java

---
+++ src/main/java/com/user/UserEntity.java
@@ -0,0 +1,13 @@
+@Entity
+  @Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String username;
+    private String password;
+    private Boolean active;
+
+  
+}
